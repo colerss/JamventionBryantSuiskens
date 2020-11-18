5f0eb247c0f4d2ad6f2c7f3d994821099b32c74f
Bryant <bryantsuiskensmast@gmail.com>
2020-11-18 16:21:36 +0100

    Test afgerond

--- JamventionWPF/JamventionDAL/DatabaseOperations.cs
+++ JamventionWPF/JamventionDAL/DatabaseOperations.cs
@@ -8,5 +8,13 @@ namespace JamventionDAL
 {
     public static class DatabaseOperations
     {
+
+        public static List<T> FullListOfType<T>() where T : class
+        {
+            using (JamventionEntities entities = new JamventionEntities())
+            {
+                return entities.Set<T>().ToList();
+            }
+        }
     }
 }
--- JamventionWPF/JamventionWPF/ViewModels/ParticipantsViewModel.cs
+++ JamventionWPF/JamventionWPF/ViewModels/ParticipantsViewModel.cs
@@ -1,16 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using GalaSoft.MvvmLight.Command;
 using JamventionWPF.Views;
+using JamventionDAL;
 
 namespace JamventionWPF.ViewModels
 {
     public class ParticipantsViewModel : BasisViewModel
     {
+        private ObservableCollection<Nationality> _nationalities;
+        private ObservableCollection<GuestRole> _guestRoles;
+
+        #region Properties
+        public ObservableCollection<Nationality> Nationalities { get 
+            {
+                return _nationalities; 
+            } 
+            set 
+            {
+                _nationalities = value;
+                NotifyPropertyChanged();
+            } 
+        }
+        public ObservableCollection<GuestRole> GuestRoles
+        {
+            get
+            {
+                return _guestRoles;
+            }
+            set
+            {
+                _guestRoles = value;
+                NotifyPropertyChanged();
+            }
+        }
+        #endregion
+
+        public ParticipantsViewModel()
+        {
+            LoadComboboxes();
+        }
+
         public override string this[string columnName]
         {
             get
@@ -18,13 +53,13 @@ namespace JamventionWPF.ViewModels
                 return "";
             }
         }
-
+        #region ICommand
         public override bool CanExecute(object parameter)
         {
             return true;
         }
+       
 
-        
 
         public override void Execute(object parameter)
         {
@@ -38,7 +73,16 @@ namespace JamventionWPF.ViewModels
                     break;
             }
         }
+        #endregion
 
+        public void LoadComboboxes()
+        {
+            List<Nationality> nationalities = DatabaseOperations.FullListOfType<Nationality>();
+            List<GuestRole> guestRoles = DatabaseOperations.FullListOfType<GuestRole>();
+            Nationalities = new ObservableCollection<Nationality>(nationalities);
+            GuestRoles = new ObservableCollection<GuestRole>(guestRoles);
+
+        }
         public void OpenRooms()
         {
             RoomsViewModel vm = new RoomsViewModel();
